8cd23b36cc3db66a28b46737d963e453d1a63786
vishnuhome6 <102293335+vishnuhome6@users.noreply.github.com>
2022-03-24 15:56:04 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,9 @@
+a=(1,2,3,4,5,6,7,8,9)
+odd_count=0
+even_count=0
+for i in a:
+	if i % 2==0:
+		even_count+=1
+	else:
+	    odd_count+=1
+print(' number of even numbers:',even_count,'\n','number of odd nummbers:',odd_count)	    	
---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+a=0
+b=1
+sum=1
+while sum < 50:
+	print(sum,end=" ")
+	sum=a + b
+	a=b
+	b=sum
+
+
+
